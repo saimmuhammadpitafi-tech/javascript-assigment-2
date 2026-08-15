a6ed7def6e99a4e32f1ae464c52ab3546eea2137
Muhammad Saim <saimmuhammadpitafi@gmail.com>
2026-08-15 09:11:17 +0500

    js assigment

---
+++ app.js
@@ -0,0 +1,56 @@
+// Question 1
+//var number = 10;
+//var result = (number + 5) * 10 / 2;
+//document.write("The result is: " + result);
+
+
+// Question 2
+//var a = 2, b = 1;
+//var result = --a - --b + ++b + b--;
+
+//document.write("a = " + a + "<br>");
+//document.write("b = " + b + "<br>");
+//document.write("result = " + result);
+
+
+// Question 3
+//var name = prompt("Enter your name:");
+//document.write("Hello " + name + "! Welcome to JavaScript.");
+
+
+// Question 5
+//var number = prompt("Enter a number:");
+
+//if (number === "" || number === null) {
+//    number = 5;
+//}
+
+//for (var i = 1; i <= 10; i++) {
+//    document.write(number + " × " + i + " = " + (number * i) + "<br>");/
+//}
+
+
+// Question 6
+//var subject1 = prompt("Enter first subject:");
+//var subject2 = prompt("Enter second subject:");
+//var subject3 = prompt("Enter third subject:");
+
+//var totalMarks = 100;
+
+//var marks1 = Number(prompt("Enter marks in " + subject1));
+//var marks2 = Number(prompt("Enter marks in " + subject2));
+//var marks3 = Number(prompt("Enter marks in " + subject3));
+
+//var totalObtained = marks1 + marks2 + marks3;
+//var total = totalMarks * 3;
+//var percentage = (totalObtained / total) * 100;
+
+//document.write("<table border='1'>");
+//document.write("<tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th></tr>");
+//document.write("<tr><td>" + subject1 + "</td><td>100</td><td>" + marks1 + "</td></tr>");
+//document.write("<tr><td>" + subject2 + "</td><td>100</td><td>" + marks2 + "</td></tr>");
+//document.write("<tr><td>" + subject3 + "</td><td>100</td><td>" + marks3 + "</td></tr>");
+//document.write("<tr><th>Total</th><th>" + total + "</th><th>" + totalObtained + "</th></tr>");
+//document.write("</table>");
+
+//document.write("<br>Percentage: " + percentage + "%");
